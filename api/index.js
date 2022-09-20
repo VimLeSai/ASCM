25ca75080a1325c74301f8bc5a84e84319271241
VimLeSai <desai.vimal250@gmail.com>
2022-09-20 17:48:01 +0530

    env update

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
-// const baseURL = "http://18.118.158.95/v1";
-const baseURL = "http://localhost:4000/v1";
+const baseURL = process.env.NEXT_PUBLIC_API_END_POINT;
 
 export const fetcher = (url, ...args) =>
   fetch(`${baseURL}${url}`, ...args).then((res) => res.json());
